c66bb00b4318d5823c3579507e679ceb8fecf86a
sahith-1792 <s3340539@live.tees.ac.uk>
2024-12-23 19:46:01 +0530

    on going updated commit

--- app/src/main/java/uk/ac/tees/mad/inv/View/Detail.kt
+++ app/src/main/java/uk/ac/tees/mad/inv/View/Detail.kt
@@ -1,5 +1,7 @@
 package uk.ac.tees.mad.inv.View
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -8,5 +10,9 @@ import uk.ac.tees.mad.inv.data.InventoryItem
 
 @Composable
 fun Detail(item: InventoryItem, viewModel: InventoryViewModel, navController: NavHostController) {
-    Text(text = item.name)
+    Scaffold {
+        Column {
+            
+        }
+    }
 }
