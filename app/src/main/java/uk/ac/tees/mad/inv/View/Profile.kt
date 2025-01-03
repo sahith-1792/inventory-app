297a070aff32d85d45164b2adf1a9437249b373a
sahith-1792 <s3340539@live.tees.ac.uk>
2025-01-03 05:30:54 +0530

    update

--- app/src/main/java/uk/ac/tees/mad/inv/View/Profile.kt
+++ app/src/main/java/uk/ac/tees/mad/inv/View/Profile.kt
@@ -1,5 +1,6 @@
 package uk.ac.tees.mad.inv.View
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -13,18 +14,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,7 +39,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -50,12 +58,19 @@ fun Profile(navController: NavHostController, viewModel: InventoryViewModel) {
     var isEditVisible by remember {
         mutableStateOf(false)
     }
-    val name = remember { mutableStateOf(user!!.name)}
-    val email = remember { mutableStateOf(user!!.email)}
+    val name = remember { mutableStateOf(user!!.name) }
+    val email = remember { mutableStateOf(user!!.email) }
+    val context = LocalContext.current
+    val sharedPreference = context.getSharedPreferences("InventoryPrefs", Context.MODE_PRIVATE)
+    var isChecked by remember {
+        mutableStateOf(sharedPreference.getBoolean("isSwitchOn", false))
+    }
     Scaffold {
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
             Column {
                 Column(
                     modifier = Modifier
@@ -113,18 +128,55 @@ fun Profile(navController: NavHostController, viewModel: InventoryViewModel) {
                     ) {
                         isEditVisible = true
                     }
+                    Spacer(modifier = Modifier.height(25.dp))
+                    Text(
+                        text = "Settings",
+                        fontFamily = Roboto,
+                        fontSize = 16.sp,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    SettingView(
+                        info = "Allow App Lock",
+                        toDisplay = "Use Fingerprint to enter into the app",
+                        icon = Icons.Default.Lock,
+                        isChecked = isChecked,
+                        onChange = {
+                            isChecked = it
+                            sharedPreference.edit().putBoolean("isSwitchOn", isChecked).apply()
+                        }
+                    )
                 }
             }
-            if (isEditVisible){
-                AlertDialog(onDismissRequest = { isEditVisible = false }){
+            if (isEditVisible) {
+                AlertDialog(onDismissRequest = { isEditVisible = false }) {
                     Card(modifier = Modifier.height(300.dp)) {
-                        Column {
+                        Column(Modifier.padding(12.dp)) {
+                            Spacer(modifier = Modifier.height(20.dp))
                             Text(text = "Name")
-                            OutlinedTextField(value = name.value, onValueChange = { name.value = it })
+                            OutlinedTextField(
+                                value = name.value,
+                                onValueChange = { name.value = it })
                             Text(text = "Email")
-                            OutlinedTextField(value = email.value, onValueChange = { email.value = it })
-                            IconButton(onClick = { /*TODO*/ }) {
-                                Text(text = "Save")
+                            OutlinedTextField(
+                                value = email.value,
+                                onValueChange = { email.value = it })
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Row {
+                                Button(
+                                    onClick = { isEditVisible = false },
+                                    shape = RoundedCornerShape(10.dp),
+                                    colors = ButtonDefaults.buttonColors(Color(0xFF00483D))
+                                ) {
+                                    Text(text = "Cancel")
+                                }
+                                Spacer(modifier = Modifier.weight(1f))
+                                Button(
+                                    onClick = { /*TODO*/ }, shape = RoundedCornerShape(10.dp),
+                                    colors = ButtonDefaults.buttonColors(Color(0xFF00483D))
+                                ) {
+                                    Text(text = "Save")
+                                }
                             }
                         }
                     }
@@ -135,19 +187,59 @@ fun Profile(navController: NavHostController, viewModel: InventoryViewModel) {
 }
 
 @Composable
-fun ProfileView(info : String ,toDisplay : String,icon : ImageVector,onClick:() -> Unit){
+fun ProfileView(info: String, toDisplay: String, icon: ImageVector, onClick: () -> Unit) {
     Row {
-        Icon(imageVector = icon, contentDescription = null, modifier = Modifier
-            .size(50.dp)
-            .align(Alignment.CenterVertically))
+        Icon(
+            imageVector = icon, contentDescription = null, modifier = Modifier
+                .size(50.dp)
+                .align(Alignment.CenterVertically)
+        )
         Spacer(modifier = Modifier.width(10.dp))
         Column {
             Text(text = info, fontFamily = Roboto, fontSize = 14.sp)
-            Text(text = toDisplay, fontFamily = Roboto, fontSize = 16.sp, fontWeight = FontWeight.SemiBold,)
+            Text(
+                text = toDisplay,
+                fontFamily = Roboto,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+            )
         }
         Spacer(modifier = Modifier.weight(1f))
-        Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = null, modifier = Modifier
-            .clickable { onClick() }
-            .align(Alignment.CenterVertically))
+        Icon(imageVector = Icons.Default.KeyboardArrowRight,
+            contentDescription = null,
+            modifier = Modifier
+                .clickable { onClick() }
+                .align(Alignment.CenterVertically))
+    }
+}
+
+@Composable
+fun SettingView(
+    info: String,
+    toDisplay: String,
+    icon: ImageVector,
+    isChecked: Boolean,
+    onChange: (Boolean) -> Unit,
+) {
+    Row {
+        Icon(
+            imageVector = icon, contentDescription = null, modifier = Modifier
+                .size(50.dp)
+                .align(Alignment.CenterVertically)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text(
+                text = info,
+                fontFamily = Roboto,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(text = toDisplay, fontFamily = Roboto, fontSize = 14.sp)
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Switch(checked = isChecked, onCheckedChange = {
+            onChange(it)
+        })
     }
 }
